a91a1908385e1f9f0a44eb4877d2cd77deac21bc
Benjamin Alright <benjaminalright@Benjamins-MacBook-Air.local>
2015-02-06 16:48:23 -0500

    Adding authorization to ensure users are logged in before they create reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-    def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  def current_user
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  def ensure_logged_in
+  	unless current_user
+  		flash[:alert] = "Please log in"
+  		redirect_to new_session_path
+  	end
   end
 
   helper_method :current_user
